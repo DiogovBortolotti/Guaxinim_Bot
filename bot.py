c9e7cde9a9cbf5e456dca062dae667d2dc0b69b9
Diogo Vieira <87249652+DiogovBortolotti@users.noreply.github.com>
2023-09-20 16:03:18 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from consultas.consulta_jogos import retona_rotacao_jogos
 
 bot = telebot.TeleBot(TOKEN_TELEGRAM)
 
-@bot.message_handler(commands=['Jogos'])
+@bot.message_handler(commands=['jogos'])
 def conferir_jogos_gratis_semana(message):
     mensagem = "ㅤㅤㅤㅤㅤ!JOGOS DA SEMANA!\n\n\n"
     for jogo in retona_rotacao_jogos():
@@ -15,7 +15,7 @@ def conferir_jogos_gratis_semana(message):
     bot.send_message(message.chat.id, mensagem)
 
 #func=lambda message: True sempre automatico
-@bot.message_handler(commands=['Ajuda', 'Comandos', 'Help', 'Start'])
+@bot.message_handler(commands=['ajuda', 'comandos', 'help', 'start'])
 def mensagem_inicial(message):
     bot.send_message(message.chat.id, f""" Ola! segue os meus comandos: \n/Jogos  - Para saber a rotação da semana """)
     #bot.reply_to(message.chat.id, f""" As Opções  """)
